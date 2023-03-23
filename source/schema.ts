3a8319781502dc750646dfda051d3342a6f5cd25
Subeom Choi <subumm1@gmail.com>
2023-03-24 00:24:55 +0900

    fix: solve issue that array of schema is used with tuple

--- source/schema.ts
+++ source/schema.ts
@@ -6,7 +6,7 @@ type TypeOf<Actual extends Schema> = (
     Actual extends 'string' ? string :
     Actual extends 'number' ? number :
     Actual extends 'boolean' ? boolean :
-    Actual extends [infer Element extends Schema] ? [TypeOf<Element>] :
+    Actual extends [infer Element extends Schema] ? TypeOf<Element>[] :
     Actual extends { [key: string]: Schema } ? { [Key in keyof Actual]: TypeOf<Actual[Key]> } :
     never
 )
